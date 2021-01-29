2b1ab0f7d1ed0d8fe90c5f045d6c3c56ef9a92dc
eneskalaycidev <76259790+eneskalaycidev@users.noreply.github.com>
2021-01-29 13:56:02 +0300

    Add files via upload

---
+++ introduction question.js
@@ -0,0 +1,16 @@
+// The code does not execute properly. Try to figure out why.
+
+/* code-question
+
+function multiply(a, b) {
+    a * b
+  }
+
+  */
+
+  // my answer
+
+  function multiply(a, b) {
+    a * b
+    return a * b
+  }
